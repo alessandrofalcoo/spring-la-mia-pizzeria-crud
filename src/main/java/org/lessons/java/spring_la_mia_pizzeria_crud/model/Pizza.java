375d4141e20238b36956fd6df7500ae107a407bc
alessandrofalcoo <alefalco.1003@gmail.com>
2025-08-05 18:42:43 +0200

    create fields for the db create getter and setter

--- src/main/java/org/lessons/java/spring_la_mia_pizzeria_crud/model/Pizza.java
+++ src/main/java/org/lessons/java/spring_la_mia_pizzeria_crud/model/Pizza.java
@@ -1,10 +1,70 @@
 package org.lessons.java.spring_la_mia_pizzeria_crud.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
 
 @Entity
 @Table(name = "pizze")
 public class Pizza {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @NotBlank(message = "name must not be null, empty or blank ")
+    private String name;
+
+    @NotBlank(message = "desc must not be null, empty or blank")
+    private String desc;
+
+    @NotBlank(message = "url must not be null, empty or blank")
+    private String url;
+
+    @NotBlank(message = "price must not be null, empty or blank")
+    private double price;
+
+    public Integer getId() {
+        return this.id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDesc() {
+        return this.desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public String getUrl() {
+        return this.url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public double getPrice() {
+        return this.price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
 
 }
