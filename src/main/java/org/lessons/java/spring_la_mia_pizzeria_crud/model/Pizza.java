22586154490d6f319aeca6372c42c0033f621ae8
alessandrofalcoo <alefalco.1003@gmail.com>
2025-08-05 18:33:59 +0200

    create main maven project with dependencies and with Pizza class create the index template

---
+++ src/main/java/org/lessons/java/spring_la_mia_pizzeria_crud/model/Pizza.java
@@ -0,0 +1,10 @@
+package org.lessons.java.spring_la_mia_pizzeria_crud.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "pizze")
+public class Pizza {
+
+}
