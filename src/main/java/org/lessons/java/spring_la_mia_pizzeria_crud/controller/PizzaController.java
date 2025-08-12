57775e817699817fd0f59ae06049b605ca86acc3
alessandrofalcoo <alefalco.1003@gmail.com>
2025-08-12 20:32:12 +0200

    create "create" and edit view

--- src/main/java/org/lessons/java/spring_la_mia_pizzeria_crud/controller/PizzaController.java
+++ src/main/java/org/lessons/java/spring_la_mia_pizzeria_crud/controller/PizzaController.java
@@ -6,10 +6,16 @@ import org.lessons.java.spring_la_mia_pizzeria_crud.repository.PizzaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import jakarta.validation.Valid;
+
 import org.lessons.java.spring_la_mia_pizzeria_crud.model.Pizza;
 
 @Controller
@@ -19,7 +25,7 @@ public class PizzaController {
     @Autowired
     private PizzaRepository repository;
 
-    @GetMapping
+    @GetMapping("/index")
     public String index(Model model) {
         List<Pizza> pizze = repository.findAll();
 
@@ -41,4 +47,31 @@ public class PizzaController {
         model.addAttribute("pizze", pizze);
         return "pizze/index";
     }
+
+    @GetMapping("/create")
+    public String create(Model model) {
+
+        model.addAttribute("pizza", new Pizza());
+
+        return "pizze/create";
+    }
+
+    @PostMapping("/create")
+    public String store(@Valid @ModelAttribute("pizza") Pizza formPizza, BindingResult bindingResult, Model model) {
+        if (bindingResult.hasErrors()) {
+            return "pizze/create";
+        }
+
+        repository.save(formPizza);
+
+        return "redirect:/pizze";
+    }
+
+    @GetMapping("/edit/{id}")
+    public String edit(@PathVariable("id") Integer id, Model model) {
+        Pizza pizza = repository.findById(id).get();
+        model.addAttribute("pizza", pizza);
+        return "pizze/edit";
+    }
+
 }
