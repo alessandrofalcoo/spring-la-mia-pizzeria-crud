096282c3b4d1ed4736ea34d3b40ee2675b1fa975
alessandrofalcoo <alefalco.1003@gmail.com>
2025-08-14 15:58:46 +0200

    add params to pizzaRepository

--- src/main/java/org/lessons/java/spring_la_mia_pizzeria_crud/repository/PizzaRepository.java
+++ src/main/java/org/lessons/java/spring_la_mia_pizzeria_crud/repository/PizzaRepository.java
@@ -8,5 +8,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface PizzaRepository extends JpaRepository<Pizza, Integer> {
     // Custom query
 
-    public List<Pizza> findByNameContaining(String name);
+    public List<Pizza> findByNameContaining(String nam);
 }
